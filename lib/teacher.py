b5e5b42e4220bee514b9835c2715535601b5d46e
David <david48kariuki@gmail.com>
2025-05-17 02:40:50 +0300

    complete

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,23 @@
-#!/usr/bin/env python
-
+import random
 from user import User
 
-import random
-
 class Teacher(User):
+    knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = Teacher.knowledge.copy()  # instance copy of knowledge list
 
     def teach(self):
-        pass
+        # Choose a random index in self.knowledge list and return the element
+        index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[index]
